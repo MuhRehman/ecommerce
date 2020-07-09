937a7c48bfa6995c39575a625f38c032edf4e8fe
askubaid <askubaid@gmail.com>
2020-07-09 22:30:20 +0500

    9th July

--- src/App.js
+++ src/App.js
@@ -1,234 +1,169 @@
-import React,{Component} from 'react';
-
+import React, { Component } from "react";
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from "./Pages/Login";
-import SignUp  from "./Pages/SignUp";
+import SignUp from "./Pages/SignUp";
 import ShoppingCart from "./Lists/ShoppingCart";
 import Products1 from "./Pages/Vegetables";
 import CreateItem from "./Pages/AddItem";
 import Products2 from "./Pages/Fruits";
 import UserInfo from "./Pages/UserInfo";
-import ItemList from './Lists/ItemList';
-import Checkout from './Pages/Checkout';
-import PageCom from './Pages/Vegetables';
-import Navbar from './Components/Navbar';
-import Cookies from  "universal-cookie";
-import Fruits from './Pages/Fruits';
-import Vegetables from './Pages/Vegetables';
-import AddItem from './Pages/AddItem';
-import ModifyItem from './Pages/ModifyItem';
-import VerifyItems from './Pages/VerifyItems';
-
-
-
+import ItemList from "./Lists/ItemList";
+import Checkout from "./Pages/Checkout";
+import PageCom from "./Pages/Vegetables";
+import Navbar from "./Components/Navbar";
+import Cookies from "universal-cookie";
+import Fruits from "./Pages/Fruits";
+import Vegetables from "./Pages/Vegetables";
+import AddItem from "./Pages/AddItem";
+import ModifyItem from "./Pages/ModifyItem";
+import VerifyItems from "./Pages/VerifyItems";
 
 /////// Remove mangakure and dog apis.  rather load the actors API
 
-
-
-
-
-
-
-
 class App extends Component {
+  state = { shoppingCartItems: [] };
 
-  state ={shoppingCartItems : []}
-    
-  constructor (props){
+  constructor(props) {
+    super(props);
 
-  super (props);
+    this.cookies = new Cookies();
 
-  this.cookies = new Cookies();
-
-  // console.log("App ka constructor");
-
-   
+    // console.log("App ka constructor");
   }
 
-componentDidMount () {
+  componentDidMount() {
+    //// check if Application reloaded ...
 
-  //// check if Application reloaded ... 
+    // console.log("App ka didMount");
 
-  // console.log("App ka didMount");
+    if (this.cookies.get("shoppingCartItems") != null) {
+      this.setState({
+        shoppingCartItems: this.cookies.get("shoppingCartItems"),
+      });
+    }
 
-  // if (this.cookies.get("shoppingCartItems") != null)
-  // {
-   
-  //   this.setState({shoppingCartItems: this.cookies.get("shoppingCartItems")});
-
-
-  // }
-
-  
-  
-  /////if yes then populate the the shoppingcart state variable from cookies
-
-
-
-
-}
-
-
-
-
-
-
-onBuy=(Item)  =>{
-
-  let  temp = this.state.shoppingCartItems;
-    
-  //  console.log(temp);
-   
-////1)  try to find if the item being clicked/bouhgt is already in the shopping cart ?
-let singleCartItem =  temp.filter((itemCart)=>  itemCart.id === Item.id)
-  
-    
-if (singleCartItem[0]) {
-  ////// 3) else if the item is already there in the shopping cart then we would only increment the quantity attribute by 1
-
-////// Yes it is already in the shopping cart  ///. third Clause
-
-singleCartItem[0].quantity += 1;
-
-singleCartItem[0].totalCost = singleCartItem[0].price * singleCartItem[0].quantity;
-//singleCartItem[0].cost += singleCartItem[0].cost;
-
-this.setState({shoppingCartItems : temp });  
-
-/// also please update the cookie variable 
-this.cookies.set("shoppingCartItems", temp);
-
-///// Javascript objects are by reference. ---- not by value /////
-
-
-
-
-} else {
-
-//// no it is for the first time being pushed.   ////// Second Clause
-//// 2) if the item is not already in the shopping cart then we want to push the item in the shopping cart
-//// with quantity attribute set to 1
-Item.quantity=1;
-Item.totalCost =Item.price;
-temp.push(Item);
-this.setState({shoppingCartItems : temp});
-  /// also please update the cookie variable 
-this.cookies.set("shoppingCartItems", temp);
-}
-
-
-
-}
-
-
-
-onIncrease=(id)  =>{
-
-  let  temp = this.state.shoppingCartItems;
-    
-  //  console.log(temp);
-   
-////1)  try to find if the item being clicked/bouhgt is already in the shopping cart ?
-let singleCartItem =  temp.filter((itemCart)=>  itemCart.id === id)
-  
-    
-if (singleCartItem[0]) {   /// for unexpected situations
-  
-
-singleCartItem[0].quantity += 1;
-
-singleCartItem[0].totalCost = singleCartItem[0].price * singleCartItem[0].quantity;
-//singleCartItem[0].cost += singleCartItem[0].cost;
-
-this.setState({shoppingCartItems : temp });  
-
-/// also please update the cookie variable 
-this.cookies.set("shoppingCartItems", temp);
-
-///// Javascript objects are by reference. ---- not by value /////
-
-
-}
-}
-onDecrease=(id)=>{
-
-
-  let  temp = this.state.shoppingCartItems;
-    
-let singleCartItem =  temp.filter((item)=> item.id == id)
- 
-if (singleCartItem[0]) {   
-  
-
-  singleCartItem[0].quantity -= 1;
-
-  if(singleCartItem[0].quantity === 0) {
-    this.onDelete(id);
-    return 
+    ///if yes then populate the the shoppingcart state variable from cookies
   }
-  
-  
-  singleCartItem[0].totalCost = singleCartItem[0].price * singleCartItem[0].quantity;
-  //singleCartItem[0].cost += singleCartItem[0].cost;
-    
-    
-  
-  this.setState({shoppingCartItems : temp });  
-  
-  /// also please update the cookie variable 
-  this.cookies.set("shoppingCartItems", temp);
 
+  onBuy = (Item) => {
+    let temp = this.state.shoppingCartItems;
 
+    //  console.log(temp);
 
-}
-}
+    ////1)  try to find if the item being clicked/bouhgt is already in the shopping cart ?
+    let singleCartItem = temp.filter((itemCart) => itemCart.id === Item.id);
 
-onDelete =(id)  =>{
+    if (singleCartItem[0]) {
+      ////// 3) else if the item is already there in the shopping cart then we would only increment the quantity attribute by 1
 
+      ////// Yes it is already in the shopping cart  ///. third Clause
 
-  let  temp = this.state.shoppingCartItems;
-    
-  //  console.log(temp);
-   
-////1)  try to find if the item being clicked/bouhgt is already in the shopping cart ?
-let newCartItems =  temp.filter((itemCart)=>  itemCart.id !== id)
-  
+      singleCartItem[0].quantity += 1;
 
+      singleCartItem[0].totalCost =
+        singleCartItem[0].price * singleCartItem[0].quantity;
+      //singleCartItem[0].cost += singleCartItem[0].cost;
 
+      this.setState({ shoppingCartItems: temp });
 
-this.setState({shoppingCartItems : newCartItems });  
+      /// also please update the cookie variable
+      this.cookies.set("shoppingCartItems", temp);
 
-/// also please update the cookie variable 
-this.cookies.set("shoppingCartItems", newCartItems);
+      ///// Javascript objects are by reference. ---- not by value /////
+    } else {
+      //// no it is for the first time being pushed.   ////// Second Clause
+      //// 2) if the item is not already in the shopping cart then we want to push the item in the shopping cart
+      //// with quantity attribute set to 1
+      Item.quantity = 1;
+      Item.totalCost = Item.price;
+      temp.push(Item);
+      this.setState({ shoppingCartItems: temp });
+      /// also please update the cookie variable
+      this.cookies.set("shoppingCartItems", temp);
+    }
+  };
 
+  onIncrease = (id) => {
+    let temp = this.state.shoppingCartItems;
 
+    //  console.log(temp);
 
+    ////1)  try to find if the item being clicked/bouhgt is already in the shopping cart ?
+    let singleCartItem = temp.filter((itemCart) => itemCart.id === id);
 
-}
+    if (singleCartItem[0]) {
+      /// for unexpected situations
 
+      singleCartItem[0].quantity += 1;
 
+      singleCartItem[0].totalCost =
+        singleCartItem[0].price * singleCartItem[0].quantity;
+      //singleCartItem[0].cost += singleCartItem[0].cost;
 
+      this.setState({ shoppingCartItems: temp });
 
+      /// also please update the cookie variable
+      this.cookies.set("shoppingCartItems", temp);
 
+      ///// Javascript objects are by reference. ---- not by value /////
+    }
+  };
+  onDecrease = (id) => {
+    let temp = this.state.shoppingCartItems;
 
+    let singleCartItem = temp.filter((item) => item.id == id);
 
+    if (singleCartItem[0]) {
+      singleCartItem[0].quantity -= 1;
 
+      if (singleCartItem[0].quantity === 0) {
+        this.onDelete(id);
+        return;
+      }
 
+      singleCartItem[0].totalCost =
+        singleCartItem[0].price * singleCartItem[0].quantity;
+      //singleCartItem[0].cost += singleCartItem[0].cost;
 
+      this.setState({ shoppingCartItems: temp });
 
-render ()
-{
+      /// also please update the cookie variable
+      this.cookies.set("shoppingCartItems", temp);
+    }
+  };
 
-  // console.log("app ka render");
-  return (
-    <div>
-    <Router>
-  <Navbar cart ={<ShoppingCart onIncrease= {this.onIncrease}  onDecrease= {this.onDecrease} onDelete={this.onDelete} cartItems={this.state.shoppingCartItems}/>}  />
+  onDelete = (id) => {
+    let temp = this.state.shoppingCartItems;
 
+    //  console.log(temp);
 
-    {/*         
+    ////1)  try to find if the item being clicked/bouhgt is already in the shopping cart ?
+    let newCartItems = temp.filter((itemCart) => itemCart.id !== id);
+
+    this.setState({ shoppingCartItems: newCartItems });
+
+    /// also please update the cookie variable
+    this.cookies.set("shoppingCartItems", newCartItems);
+  };
+
+  render() {
+    // console.log("app ka render");
+    return (
+      <div>
+        <Router>
+          <Navbar
+            cart={
+              <ShoppingCart
+                onIncrease={this.onIncrease}
+                onDecrease={this.onDecrease}
+                onDelete={this.onDelete}
+                cartItems={this.state.shoppingCartItems}
+              />
+            }
+          />
+
+          {/*         
     
       router - AppLayout component 
             
@@ -242,31 +177,52 @@ render ()
   
     */}
 
+          <Switch>
+            <Route exact path="/">
+              <Login></Login>{" "}
+            </Route>
+            <Route exact path="/updateitem">
+              {" "}
+              <ModifyItem />{" "}
+            </Route>
+            <Route exact path="/additem">
+              {" "}
+              <AddItem />{" "}
+            </Route>
+            <Route exact path="/vegetables">
+              {" "}
+              <Vegetables onBuy={this.onBuy} />{" "}
+            </Route>
+            <Route exact path="/fruits">
+              {" "}
+              <Fruits onBuy={this.onBuy} />{" "}
+            </Route>
 
-<Switch>
-            <Route exact path='/'><Login></Login> </Route> 
-            <Route exact path='/updateitem'>  <ModifyItem />  </Route> 
-            <Route exact path='/additem'>  <AddItem />  </Route> 
-            <Route exact path='/vegetables'>  <Vegetables onBuy={this.onBuy} />  </Route> 
-            <Route exact path='/fruits'>  <Fruits onBuy={this.onBuy} />  </Route> 
-            
-           
-            <Route path="/sign-in"    > <Login></Login></Route> 
-            <Route path="/sign-up"  > <SignUp></SignUp></Route> 
-            <Route path="/userinfo" > <UserInfo></UserInfo></Route> 
-            <Route path="/checkout"  ><Checkout></Checkout></Route> 
-            <Route path="/verify"  ><VerifyItems></VerifyItems></Route> 
+            <Route path="/sign-in">
+              {" "}
+              <Login></Login>
+            </Route>
+            <Route path="/sign-up">
+              {" "}
+              <SignUp></SignUp>
+            </Route>
+            <Route path="/userinfo">
+              {" "}
+              <UserInfo></UserInfo>
+            </Route>
+            <Route path="/checkout">
+              <Checkout></Checkout>
+            </Route>
+            <Route path="/verify">
+              <VerifyItems></VerifyItems>
+            </Route>
           </Switch>
 
-
-
-
-     {/* <PageCom></PageCom> */}  
-     </Router>
-     </div>
-     
-  );
-}
+          {/* <PageCom></PageCom> */}
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Pages/ModifyItem.js
+++ src/Pages/ModifyItem.js
@@ -4,21 +4,18 @@ import axios from "axios";
 import Alert from "../Components/Alert";
 import SmartTable from "../Lists/SmartTable";
 
-
 export default class ModifyItem extends Component {
-  state={};
+  state = {};
   constructor(props) {
     super(props);
 
-    this.state= this.stateReset();
-    this.state.apiData=[];
-   
+    this.state = this.stateReset();
+    this.state.apiData = [];
   }
-
 
   stateReset = () => {
     return {
-      id:"",
+      id: "",
       title: "",
       price: "150",
       currency: "Rs",
@@ -28,36 +25,35 @@ export default class ModifyItem extends Component {
       image: {},
       ready: false,
       isLoading: false,
-      alertAppear:false,
-      alertType:"",
-      alertMsg:"",
-      imageUrl:""
+      alertAppear: false,
+      alertType: "",
+      alertMsg: "",
+      imageUrl: "",
+    };
+  };
 
+  componentDidMount() {
+    // Make a request for a user with a given ID
+    axios
+      .get("https://mangakure.com/dummies")
+      .then((response) => {
+        // handle success
+        // console.log(response.data);
+        this.setState({ apiData: response.data });
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      });
+  }
 
-         } 
-  } 
-
-     componentDidMount () {
- // Make a request for a user with a given ID
-  axios.get('https://mangakure.com/dummies')
-  .then((response)=>{
-    // handle success
-    // console.log(response.data);
-      this.setState({apiData :response.data});
-  })
-  .catch((error)=>{
-    // handle error
-    console.log(error);
-  })
-     }
-  
-  postDataForUpdate = () => {
+  postDataForUpdate = async () => {
     //// post the data to the Api
 
     let fields = {};
 
     /////////// What are the Data fields
-  // 
+    //
     fields["title"] = this.state.title;
     fields["description"] = this.state.description;
     fields["price"] = this.state.price;
@@ -70,39 +66,25 @@ export default class ModifyItem extends Component {
     const formData = new FormData(); /// create a new form Data
 
     //////////// Append the file or files.
-    
-    ////// aik tareeqa  ////  to skip the following statement if user input is empty.
 
-    if (this.state.image.url)
-    {
-      formData.append("data", JSON.stringify(fields)); /// the second attribute strapi requires is "data" where we have all the dataFields
-      
+    if (this.state.image instanceof Blob) {
+      formData.append("files.image", this.state.image, this.state.image.name);
+      formData.append("data", JSON.stringify(fields));
     }
-  
-   else  
 
-   {
-    formData.append("files.image", this.state.image, this.state.image.fileName); /// Strapi accepts files on "files.AttributeName"
-    ////// Append the Data fields..
-    formData.append("data", JSON.stringify(fields)); /// the second attribute strapi requires is "data" where we have all the dataFields
-    
+    ////// aik tareeqa  ////  to skip uploading the picture, if user input is empty.
+    else formData.append("data", JSON.stringify(fields)); ///  Only update the data///
 
-   }
-  
-    
-    ////// second method   //// to convert this.state.image into a blob ////
+    ////// second method   //// to convert this.state.image into a blob //// less effecient
 
+    ///  {
+    //   let blob = await fetch(this.state.imageUrl).then((r) => r.blob());  /// convert img url to blob
+    //   formData.append("files.image", blob, this.state.image.name);
+    //   ////// Append the Data fields..
+    //   formData.append("data", JSON.stringify(fields));
+    //   }
 
-
-    // const blob = new Blob(this.state.image);
-
-
-    // formData.append("files.image", blob, this.state.image.name); /// Strapi accepts files on "files.AttributeName"
-    // ////// Append the Data fields..
-    // formData.append("data", JSON.stringify(fields)); /// the second attribute strapi requires is "data" where we have all the dataFields
-
-    //files.image = {Blob}
-    //data={data that we have collected}
+    ///////////////////////////////////
 
     axios({
       method: "put",
@@ -110,41 +92,20 @@ export default class ModifyItem extends Component {
       data: formData, ////// data is the attribute for Axios for whatever object to be posted.
     })
       .then((success) => {
-        
-        let newObj =this.stateReset();
-        
-        newObj.alertMsg ="Record has been successfully Updated";
-        newObj.alertType= "success";
-        newObj.alertAppear= true;
+        let newObj = this.stateReset();
 
-        ////// kuw na... apiData  .... sirf us record ko update kar den..
+        newObj.alertMsg = "Record has been successfully Updated";
+        newObj.alertType = "success";
+        newObj.alertAppear = true;
 
-        ////success.data //// updated object
-        
-        
+        newObj.apiData = this.state.apiData.map((item) => {
+          if (item.id === success.data.id) return success.data;
+          else return item;
+        });
 
-        newObj.apiData =this.state.apiData.map((item)=>{ 
-
-          if (item.id ===success.data.id)
-          return success.data;
-          else
-          return item;
-           });
-
-                
-        //// table ka data /// apiData
-
-
-        ///////////
-
-
-        console.log(success);    /////// update the table content ///
-        
         this.setState(newObj);
-          
-        })
-        /// display that record has been uploaded.
-      
+      })
+
       .catch((err) => {
         this.setState({
           isLoading: false,
@@ -157,50 +118,36 @@ export default class ModifyItem extends Component {
       });
   };
 
-
   onSubmit = (e) => {
     e.preventDefault();
     ///// verify that all the inputs are valid  .... VALIDATION ...
     this.setState({ ready: true });
 
-  
-
     let form = e.currentTarget;
-       
+
     if (form.checkValidity()) {
       this.setState({ isLoading: true }); /// display a spinner while data is uploading.   //// Once uploaded.. reset the form
       this.postDataForUpdate(); //// if all goood .... /// update the data to the Api
     }
   };
 
+  loadRecord = (id) => {
+    let selectedRecord = this.state.apiData.filter((item) => item.id === id);
 
-loadRecord = (id)=>{
-  
-   let  selectedRecord = this.state.apiData.filter((item)=> item.id === id );
-      
-      
-
-  this.setState ({
-    id,
-    title: selectedRecord[0].title,
+    this.setState({
+      id,
+      title: selectedRecord[0].title,
       price: selectedRecord[0].price,
-      currency:selectedRecord[0].currency,
+      currency: selectedRecord[0].currency,
       unit: selectedRecord[0].unit,
       category: selectedRecord[0].category,
       description: selectedRecord[0].description,
       image: selectedRecord[0].image,
-      imageUrl: `https://mangakure.com/${selectedRecord[0].image.url}`
-     
-  });
+      imageUrl: `https://mangakure.com/${selectedRecord[0].image.url}`,
+    });
 
-
-  console.log(selectedRecord[0].image.name)
-
-
-}
-
-
-
+    console.log(selectedRecord[0].image.name);
+  };
 
   render() {
     if (this.state.isLoading == true)
@@ -210,9 +157,6 @@ loadRecord = (id)=>{
           <Spinner animation="grow" size={"lg"} variant="primary"></Spinner>
         </center>
       );
-
-
-
 
     return (
       <div>
@@ -231,8 +175,6 @@ loadRecord = (id)=>{
             borderRadius: "15px",
           }}
         >
-         
-
           {/* ///// whenever we call this component. it will display a message  
 
 
@@ -242,7 +184,7 @@ loadRecord = (id)=>{
            type= "success", */}
 
           <Form.Group controlId="formGridEmail">
-          <Form.Group  controlId="formGridPassword">
+            <Form.Group controlId="formGridPassword">
               <Form.Label>
                 Category <span style={{ color: "red" }}>*</span>
               </Form.Label>
@@ -258,9 +200,8 @@ loadRecord = (id)=>{
                 type="text"
                 placeholder=""
               >
-                <option  >Vegetable</option>
-                <option  >Fruits</option>
-                
+                <option>Vegetable</option>
+                <option>Fruits</option>
               </Form.Control>
             </Form.Group>
             <Form.Label>
@@ -287,7 +228,7 @@ loadRecord = (id)=>{
               Value is required. Please input a Valid Title{" "}
             </Form.Control.Feedback>
           </Form.Group>
-     
+
           <Form.Row>
             <Form.Group as={Col} controlId="formGridPassword">
               <Form.Label>
@@ -381,21 +322,30 @@ loadRecord = (id)=>{
             <Form.Label className={"form-label col-form-label"}>
               {" "}
               Item Picture{" "}
-            </Form.Label >
-                 {this.state.imageUrl !== "" ?
-                 <img  className="form-control mb-2"  style={{height:"120px",margin:"auto",objectFit:"contain"}} 
-                 src={this.state.imageUrl}></img>
-                 :"" 
-                 }
-            
-            <Form.Control 
-              
+            </Form.Label>
+            {this.state.imageUrl !== "" ? (
+              <img
+                className="form-control mb-2"
+                style={{
+                  height: "120px",
+                  margin: "auto",
+                  objectFit: "contain",
+                }}
+                src={this.state.imageUrl}
+              ></img>
+            ) : (
+              ""
+            )}
+
+            <Form.Control
               name="image"
               //value={this.state.image.name}
               onChange={(e) => {
-
-                this.setState({ [e.target.name]: e.target.files[0], imageUrl: URL.createObjectURL(e.target.files[0]) });
-              }}  
+                this.setState({
+                  [e.target.name]: e.target.files[0],
+                  imageUrl: URL.createObjectURL(e.target.files[0]),
+                });
+              }}
               type="file"
             />
             {/* <Form.Control required name="file" type="text" value={this.state.img} onChange={(e) => {this.setState({[e.target.name] : e.target.value})}} as="text" style={{display:"none"}} /> */}
@@ -420,13 +370,15 @@ loadRecord = (id)=>{
               });
             }}
           ></Alert>
-          <Button className="mt-4" variant="primary" type="submit" >
+          <Button className="mt-4" variant="primary" type="submit">
             Save
           </Button>
         </Form>
 
-        <SmartTable onModify ={this.loadRecord}  content={this.state.apiData}></SmartTable>
-
+        <SmartTable
+          onModify={this.loadRecord}
+          content={this.state.apiData}
+        ></SmartTable>
       </div>
     );
   }
